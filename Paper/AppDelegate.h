4734807771a44e55c52aa94c07e251d71501ff92
mattstudio <matt@usertesting.com>
2014-06-18 21:26:28 -0700

    Initial Commit

---
+++ Paper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Paper
+//
+//  Created by Matt Mitchell on 6/18/14.
+//  Copyright (c) 2014 Matt Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
